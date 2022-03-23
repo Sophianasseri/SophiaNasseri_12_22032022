359d3c6c514557bf1f19d0c20e6b4a4e5310765e
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-03-23 20:16:13 +0100

    get profile page data from id

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { mainData } from '../../mockedData/mockedData';
 
 function User() {
+  const { id } = useParams();
+  const [user, setUser] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user === undefined) {
+      const element = mainData.find((elt) => elt.id.toString() === id);
+      // Redirect to 404 error page if user change id parameter to an undefined one
+      if (element === undefined) {
+        navigate('/404');
+      } else {
+        setUser(element);
+      }
+    }
+  }, []);
   return (
-    <div>
-      <h1>User</h1>
-    </div>
+    <main>
+      {user ? (
+        <div key={user.id}>
+          <h1>
+            Bonjour,
+            {user.userInfos.firstName}
+          </h1>
+        </div>
+      ) : (
+        <p>Chargement...</p>
+      )}
+    </main>
   );
 }
 
