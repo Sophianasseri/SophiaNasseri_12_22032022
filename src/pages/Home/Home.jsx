0ff4b5640d78604dc8c0cc4a6d806e3993a43be3
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-04-29 13:33:16 +0200

    style: home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './home.module.css';
@@ -7,8 +8,17 @@ function Home() {
     <main className={styles.main}>
       <div className={styles.container}>
         <div>
-          <Link to="user/12">Utilisateur 12</Link>
-          <Link to="user/18">Utilisateur 18</Link>
+          <h1 className={styles.title}>
+            Bienvenue sur <span>SportSee</span>!
+          </h1>
+          <p>
+            Vous pouvez retrouver un échantillon de données pour 2 utilisateurs
+            :
+          </p>
+          <div className={styles.links}>
+            <Link to="user/12">Utilisateur 12</Link>
+            <Link to="user/18">Utilisateur 18</Link>
+          </div>
         </div>
       </div>
     </main>
